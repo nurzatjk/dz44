773157c84027a70e7b66d646927b154db00798e3
nurzat <nurzattalajbekova85@gmail.com>
2022-11-28 21:29:17 +0600

    ллооо

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,137 @@
+import java.util.Random;
+
 public class Main {
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+    public static String bossDefenceType;
+    public static int[] heroesHealth = {280, 270, 250, 220};
+    public static int[] heroesDamage = {10, 15, 20, 0};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic"};
+
+    public static int roundNumber;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printStatistics();
+
+        while (!isGameFinished()) {
+            round();
+        }
     }
+
+    public static void chooseBossDefence() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(heroesAttackType.length); // 0,1,2
+        bossDefenceType = heroesAttackType[randomIndex];
+    }
+
+
+
+
+    public static void round() {
+        roundNumber++;
+        chooseBossDefence();
+        bossHits();
+        heroesHit();
+        printStatistics();
+        medicSkill();
+
+    }
+
+    private static void medicSkill() {
+        for (int i = 0; i <heroesHealth.length ; i++) {
+            if (heroesAttackType[i].equals("Medic")){
+                continue;
+            }else if (heroesHealth[i] >0 && heroesHealth[i] <100 && heroesHealth[3] >0){
+                heroesHealth[i]=+12;
+                System.out.println("Медик вылечил: " + heroesAttackType[i]);
+                break;
+            }
+
+        }
+    }
+
+
+    public static void bossHits() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                if (heroesHealth[i] - bossDamage < 0) {
+                    heroesHealth[i] = 0;
+                } else {
+                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                }
+            }
+        }
+    }
+
+    public static void heroesHit() {
+        for (int i = 0; i < heroesDamage.length; i++) {
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                int damage = heroesDamage[i];
+                if (bossDefenceType == heroesAttackType[i]) {
+                    Random random = new Random();
+                    int coeff = random.nextInt(9) + 2; // 2,3,4,5,6,7,8,9,10
+                    damage = heroesDamage[i] * coeff;
+                    System.out.println("Critical damage: " + damage);
+                }
+                if (bossHealth - damage < 0) {
+                    bossHealth = 0;
+                } else {
+                    bossHealth = bossHealth - damage;
+                }
+            }
+        }
+    }
+
+    public static void printStatistics() {
+        System.out.println("ROUND " + roundNumber + " ----------------");
+        /*String defence;
+        if (bossDefenceType == null) {
+            defence = "No defence";
+        } else {
+            defence = bossDefenceType;
+        }*/
+        System.out.println("Boss health: " + bossHealth + " damage: " + bossDamage
+                + " defence: "
+                + (bossDefenceType == null ? "No defence" : bossDefenceType));
+        for (int i = 0; i < heroesHealth.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: "
+                    + heroesHealth[i] + " damage: " + heroesDamage[i]);
+        }
+    }
+
+    public static boolean isGameFinished() {
+        if (bossHealth <= 0) {
+            System.out.println("Heroes won!");
+            return true;
+        }
+        /*if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0 && heroesHealth[2] <= 0) {
+            System.out.println("Boss won!");
+            return true;
+        }
+        return false;*/
+        boolean allHeroesDead = true;
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("Boss won!");
+        }
+        return allHeroesDead;
+    }
+    public static void setHeroesHealth (){
+    }Random random = new Random();
+    int rand = random.nextInt(heroesHealth.length);//медик должен
+
+    //медик добавлен
+    ///способность лечить  каждого раунда на N количество ед здоровья только одного из членов команды здоровье >100ед
+    //оживлять не может
+    //лечить не может
+    //
+    ///
+    //
+
+
 }
